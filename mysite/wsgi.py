ce9717a0eb28ee53edc86f3f47996e95bc50da63
Richard Ho <skynet.tw@gmail.com>
2022-08-17 16:30:28 +0800

    for merge

--- mysite/wsgi.py
+++ mysite/wsgi.py
@@ -7,10 +7,12 @@ For more information on this file, see
 https://docs.djangoproject.com/en/4.0/howto/deployment/wsgi/
 """
 
-import os
+import os, sys
 
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
 
 application = get_wsgi_application()
+sys.path.append("/var/www/tang/aipro4/aiproject/")
+sys.path.append("/var/www/tang/aipro4/static3/")
